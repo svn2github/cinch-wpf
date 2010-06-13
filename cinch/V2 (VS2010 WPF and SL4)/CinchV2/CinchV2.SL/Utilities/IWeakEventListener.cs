6c60ccf920a64a666186bb4e4d40716c34e391a0
SND\sachabarber_cp <SND\sachabarber_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2010-06-13 15:44:42 +0000

    Attempting to add CinchV2
    git-svn-id: https://cinch.svn.codeplex.com/svn@47259 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

---
+++ cinch/V2 (VS2010 WPF and SL4)/CinchV2/CinchV2.SL/Utilities/IWeakEventListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+
+
+namespace System.Windows
+{
+    // Summary:
+    //     Provides event listening support for classes that expect to receive events
+    //     through the WeakEvent pattern and a System.Windows.WeakEventManager.
+    public interface IWeakEventListener
+    {
+        // Summary:
+        //     Receives events from the centralized event manager.
+        //
+        // Parameters:
+        //   managerType:
+        //     The type of the System.Windows.WeakEventManager calling this method.
+        //
+        //   sender:
+        //     Object that originated the event.
+        //
+        //   e:
+        //     Event data.
+        //
+        // Returns:
+        //     true if the listener handled the event. It is considered an error by the
+        //     System.Windows.WeakEventManager handling in WPF to register a listener for
+        //     an event that the listener does not handle. Regardless, the method should
+        //     return false if it receives an event that it does not recognize or handle.
+        bool ReceiveWeakEvent(Type managerType, object sender, EventArgs e);
+    }
+}
