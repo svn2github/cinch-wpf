e0ae9e9a4f32127d83bcc758c345ab865433c885
SND\sachabarber_cp <SND\sachabarber_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2011-01-02 08:36:07 +0000

    Forgot to add ICinchDisposable to SVN source control. Grrr…Ooops sorry
    git-svn-id: https://cinch.svn.codeplex.com/svn@55703 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

---
+++ cinch/V2 (VS2010 WPF and SL)/CinchV2/ViewModels/ICinchDisposable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cinch
+{
+    public interface ICinchDisposable
+    {
+        // Summary:
+        //     Performs Cinch related resource cleaning, such as unhooking
+        //     Mediator message registration, etc etc
+        void Dispose();
+    }
+}
