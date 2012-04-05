d42131706d5f6d6df7b7cf0b0a9d94dfb785f768
SND\sachabarber_cp <SND\sachabarber_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2012-04-05 06:22:22 +0000

    Adding enhancements to the WPF message box service based on Cinch user Michel Renauds comments. In fact Michel provided the work I just reviewed it and added his extra methods.
    In his words this is why they needed adding
    
    Early on when we started using Cinch, we noticed some limitations in the message box service, namely hardcoded captions (we sometimes need something more explicit, and the apps are bilingual too) and inability to set a specific button as the default button (e.g. having "Cancel" as the default instead of "Ok" ). I ended up adding more methods to the service to take care of that.
    
    git-svn-id: https://cinch.svn.codeplex.com/svn@68724 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

--- cinch/V2 (VS2010 WPF and SL)/CinchV2.WPF/Services/Implementation/WPFMessageBoxService.cs
+++ cinch/V2 (VS2010 WPF and SL)/CinchV2.WPF/Services/Implementation/WPFMessageBoxService.cs
@@ -26,6 +26,16 @@ namespace Cinch
         }
 
         /// <summary>
+        /// Displays an error dialog with a given message and caption.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        public void ShowError(string message, string caption)
+        {
+            ShowMessage(message, caption, CustomDialogIcons.Stop);
+        }
+
+        /// <summary>
         /// Displays an error dialog with a given message.
         /// </summary>
         /// <param name="message">The message to be displayed.</param>
@@ -38,9 +48,29 @@ namespace Cinch
         /// Displays an error dialog with a given message.
         /// </summary>
         /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        public void ShowInformation(string message, string caption)
+        {
+            ShowMessage(message, caption, CustomDialogIcons.Information);
+        }
+
+        /// <summary>
+        /// Displays an error dialog with a given message.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
         public void ShowWarning(string message)
         {
             ShowMessage(message, "Warning", CustomDialogIcons.Warning);
+        }
+
+        /// <summary>
+        /// Displays an error dialog with a given message.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        public void ShowWarning(string message, string caption)
+        {
+            ShowMessage(message, caption, CustomDialogIcons.Warning);
         }
 
         /// <summary>
@@ -55,6 +85,31 @@ namespace Cinch
         }
 
         /// <summary>
+        /// Displays a Yes/No dialog and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowYesNo(string message, string caption, CustomDialogIcons icon)
+        {
+            return ShowQuestionWithButton(message, caption, icon, CustomDialogButtons.YesNo);
+        }
+
+        /// <summary>
+        /// Displays a Yes/No dialog with a default button selected, and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <param name="defaultResult">Default result for the message box</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowYesNo(string message, string caption, CustomDialogIcons icon, CustomDialogResults defaultResult)
+        {
+            return ShowQuestionWithButton(message, caption, icon, CustomDialogButtons.YesNo, defaultResult);
+        }
+
+        /// <summary>
         /// Displays a Yes/No/Cancel dialog and returns the user input.
         /// </summary>
         /// <param name="message">The message to be displayed.</param>
@@ -63,6 +118,32 @@ namespace Cinch
         public CustomDialogResults ShowYesNoCancel(string message, CustomDialogIcons icon)
         {
             return ShowQuestionWithButton(message, icon, CustomDialogButtons.YesNoCancel);
+        }
+
+
+        /// <summary>
+        /// Displays a Yes/No/Cancel dialog and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowYesNoCancel(string message, string caption, CustomDialogIcons icon)
+        {
+            return ShowQuestionWithButton(message, caption, icon, CustomDialogButtons.YesNoCancel);
+        }
+
+        /// <summary>
+        /// Displays a Yes/No/Cancel dialog with a default button selected, and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <param name="defaultResult">Default result for the message box</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowYesNoCancel(string message, string caption, CustomDialogIcons icon, CustomDialogResults defaultResult)
+        {
+            return ShowQuestionWithButton(message, caption, icon, CustomDialogButtons.YesNoCancel, defaultResult);
         }
 
         /// <summary>
@@ -75,6 +156,31 @@ namespace Cinch
         {
             return ShowQuestionWithButton(message, icon, CustomDialogButtons.OKCancel);
         }
+
+        /// <summary>
+        /// Displays a OK/Cancel dialog and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowOkCancel(string message, string caption, CustomDialogIcons icon)
+        {
+            return ShowQuestionWithButton(message, caption, icon, CustomDialogButtons.OKCancel);
+        }
+
+        /// <summary>
+        /// Displays a OK/Cancel dialog with a default result selected, and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <param name="defaultResult">Default result for the message box</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowOkCancel(string message, string caption, CustomDialogIcons icon, CustomDialogResults defaultResult)
+        {
+            return ShowQuestionWithButton(message, caption, icon, CustomDialogButtons.OKCancel, defaultResult);
+        }
         #endregion
 
         #region Private Methods
@@ -82,11 +188,11 @@ namespace Cinch
         /// Shows a standard System.Windows.MessageBox using the parameters requested
         /// </summary>
         /// <param name="message">The message to be displayed.</param>
-        /// <param name="heading">The heading to be displayed</param>
+        /// <param name="caption">The heading to be displayed</param>
         /// <param name="icon">The icon to be displayed.</param>
-        private void ShowMessage(string message, string heading, CustomDialogIcons icon)
+        private void ShowMessage(string message, string caption, CustomDialogIcons icon)
         {
-            MessageBox.Show(message, heading, MessageBoxButton.OK, GetImage(icon));
+            MessageBox.Show(message, caption, MessageBoxButton.OK, GetImage(icon));
         }
 
 
@@ -107,6 +213,50 @@ namespace Cinch
         {
             MessageBoxResult result = MessageBox.Show(message, "Please confirm...",
                 GetButton(button), GetImage(icon));
+            return GetResult(result);
+        }
+
+
+        /// <summary>
+        /// Shows a standard System.Windows.MessageBox using the parameters requested
+        /// but will return a translated result to enable adhere to the IMessageBoxService
+        /// implementation required. 
+        /// 
+        /// This abstraction allows for different frameworks to use the same ViewModels but supply
+        /// alternative implementations of core service interfaces
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <param name="button"></param>
+        /// <returns>CustomDialogResults results to use</returns>
+        private CustomDialogResults ShowQuestionWithButton(string message, string caption,
+            CustomDialogIcons icon, CustomDialogButtons button)
+        {
+            MessageBoxResult result = MessageBox.Show(message, caption,
+                GetButton(button), GetImage(icon));
+            return GetResult(result);
+        }
+
+        /// <summary>
+        /// Shows a standard System.Windows.MessageBox using the parameters requested
+        /// but will return a translated result to enable adhere to the IMessageBoxService
+        /// implementation required. 
+        /// 
+        /// This abstraction allows for different frameworks to use the same ViewModels but supply
+        /// alternative implementations of core service interfaces
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <param name="button"></param>
+        /// <param name="defaultResult">Default result for the message box</param>
+        /// <returns>CustomDialogResults results to use</returns>
+        private CustomDialogResults ShowQuestionWithButton(string message, string caption,
+            CustomDialogIcons icon, CustomDialogButtons button, CustomDialogResults defaultResult)
+        {
+            MessageBoxResult result = MessageBox.Show(message, caption,
+                GetButton(button), GetImage(icon), GetResult(defaultResult));
             return GetResult(result);
         }
 
@@ -207,7 +357,37 @@ namespace Cinch
             return customDialogResults;
         }
 
+        /// <summary>
+        /// Translates a CustomDialogResults into a standard WPF System.Windows.MessageBox MessageBoxResult
+        /// This abstraction allows for different frameworks to use the same ViewModels but supply
+        /// alternative implementations of core service interfaces
+        /// </summary>
+        /// <param name="result">The CustomDialogResults</param>
+        /// <returns>The standard WPF System.Windows.MessageBox MessageBoxResult results to use</returns>
+        private MessageBoxResult GetResult(CustomDialogResults result)
+        {
+            MessageBoxResult customDialogResults = MessageBoxResult.None;
 
+            switch (result)
+            {
+                case CustomDialogResults.Cancel:
+                    customDialogResults = MessageBoxResult.Cancel;
+                    break;
+                case CustomDialogResults.No:
+                    customDialogResults = MessageBoxResult.No;
+                    break;
+                case CustomDialogResults.None:
+                    customDialogResults = MessageBoxResult.None;
+                    break;
+                case CustomDialogResults.OK:
+                    customDialogResults = MessageBoxResult.OK;
+                    break;
+                case CustomDialogResults.Yes:
+                    customDialogResults = MessageBoxResult.Yes;
+                    break;
+            }
+            return customDialogResults;
+        }
         #endregion
 
     }
--- cinch/V2 (VS2010 WPF and SL)/CinchV2.WPF/Services/Test Implementations/TestMessageBoxService.cs
+++ cinch/V2 (VS2010 WPF and SL)/CinchV2.WPF/Services/Test Implementations/TestMessageBoxService.cs
@@ -71,10 +71,32 @@ namespace Cinch
         }
 
         /// <summary>
+        /// Shows an error message with a custom caption
+        /// </summary>
+        /// <param name="message">The error message</param>
+        /// <param name="caption">The caption</param>
+        public void ShowError(string message, string caption)
+        {
+            //Nothing to do, as there will never be a UI
+            //as we are testing the VMs
+        }
+
+        /// <summary>
         /// Does nothing, as nothing required for testing
         /// </summary>
         /// <param name="message">The message to be displayed.</param>
         public void ShowInformation(string message)
+        {
+            //Nothing to do, as there will never be a UI
+            //as we are testing the VMs
+        }
+
+        /// <summary>
+        /// Shows an information message with a custom caption
+        /// </summary>
+        /// <param name="message">The information message</param>
+        /// <param name="caption">The caption</param>
+        public void ShowInformation(string message, string caption)
         {
             //Nothing to do, as there will never be a UI
             //as we are testing the VMs
@@ -90,6 +112,16 @@ namespace Cinch
             //as we are testing the VMs        
         }
 
+        /// <summary>
+        /// Shows an warning message with a custom caption
+        /// </summary>
+        /// <param name="message">The warning message</param>
+        /// <param name="caption">The caption</param>
+        public void ShowWarning(string message, string caption)
+        {
+            //Nothing to do, as there will never be a UI
+            //as we are testing the VMs      
+        }
 
         /// <summary>
         /// Returns the next Dequeue ShowYesNo response expected. See the tests for 
@@ -99,6 +131,47 @@ namespace Cinch
         /// <param name="icon">The icon to be displayed.</param>
         /// <returns>User selection.</returns>
         public CustomDialogResults ShowYesNo(string message, CustomDialogIcons icon)
+        {
+            if (ShowYesNoResponders.Count == 0)
+                throw new ApplicationException(
+                    "TestMessageBoxService ShowYesNo method expects a Func<CustomDialogResults> callback \r\n" +
+                    "delegate to be enqueued for each Show call");
+            else
+            {
+                Func<CustomDialogResults> responder = ShowYesNoResponders.Dequeue();
+                return responder();
+            }
+        }
+
+        /// <summary>
+        /// Displays a Yes/No dialog with a custom caption, and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowYesNo(string message, string caption, CustomDialogIcons icon)
+        {
+            if (ShowYesNoResponders.Count == 0)
+                throw new ApplicationException(
+                    "TestMessageBoxService ShowYesNo method expects a Func<CustomDialogResults> callback \r\n" +
+                    "delegate to be enqueued for each Show call");
+            else
+            {
+                Func<CustomDialogResults> responder = ShowYesNoResponders.Dequeue();
+                return responder();
+            }
+        }
+
+        /// <summary>
+        /// Displays a Yes/No dialog with a default button selected, and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <param name="defaultResult">Default result for the message box</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowYesNo(string message, string caption, CustomDialogIcons icon, CustomDialogResults defaultResult)
         {
             if (ShowYesNoResponders.Count == 0)
                 throw new ApplicationException(
@@ -132,6 +205,47 @@ namespace Cinch
         }
 
         /// <summary>
+        /// Displays a Yes/No/Cancel dialog with a custom caption and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowYesNoCancel(string message, string caption, CustomDialogIcons icon)
+        {
+            if (ShowYesNoCancelResponders.Count == 0)
+                throw new ApplicationException(
+                    "TestMessageBoxService ShowYesNoCancel method expects a Func<CustomDialogResults> callback \r\n" +
+                    "delegate to be enqueued for each Show call");
+            else
+            {
+                Func<CustomDialogResults> responder = ShowYesNoCancelResponders.Dequeue();
+                return responder();
+            }
+        }
+
+        /// <summary>
+        /// Displays a Yes/No/Cancel dialog with a default button selected, and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// /// <param name="defaultResult">Default result for the message box</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowYesNoCancel(string message, string caption, CustomDialogIcons icon, CustomDialogResults defaultResult)
+        {
+            if (ShowYesNoCancelResponders.Count == 0)
+                throw new ApplicationException(
+                    "TestMessageBoxService ShowYesNoCancel method expects a Func<CustomDialogResults> callback \r\n" +
+                    "delegate to be enqueued for each Show call");
+            else
+            {
+                Func<CustomDialogResults> responder = ShowYesNoCancelResponders.Dequeue();
+                return responder();
+            }
+        }
+
+        /// <summary>
         /// Returns the next Dequeue ShowOkCancel response expected. See the tests for 
         /// the Func callback expected values
         /// </summary>
@@ -151,6 +265,46 @@ namespace Cinch
             }
         }
 
+        /// <summary>
+        /// Displays a OK/Cancel dialog with a custom caption and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowOkCancel(string message, string caption, CustomDialogIcons icon)
+        {
+            if (ShowOkCancelResponders.Count == 0)
+                throw new ApplicationException(
+                    "TestMessageBoxService ShowOkCancel method expects a Func<CustomDialogResults> callback \r\n" +
+                    "delegate to be enqueued for each Show call");
+            else
+            {
+                Func<CustomDialogResults> responder = ShowOkCancelResponders.Dequeue();
+                return responder();
+            }
+        }
+
+        /// <summary>
+        /// Displays a OK/Cancel dialog with a default button selected, and returns the user input.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="caption">The caption of the message box window</param>
+        /// <param name="icon">The icon to be displayed.</param>
+        /// <param name="defaultResult">Default result for the message box</param>
+        /// <returns>User selection.</returns>
+        public CustomDialogResults ShowOkCancel(string message, string caption, CustomDialogIcons icon, CustomDialogResults defaultResult)
+        {
+            if (ShowOkCancelResponders.Count == 0)
+                throw new ApplicationException(
+                    "TestMessageBoxService ShowOkCancel method expects a Func<CustomDialogResults> callback \r\n" +
+                    "delegate to be enqueued for each Show call");
+            else
+            {
+                Func<CustomDialogResults> responder = ShowOkCancelResponders.Dequeue();
+                return responder();
+            }
+        }
         #endregion
     }
 }
